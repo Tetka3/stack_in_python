560ba82fa5297d3cc1b3eb58920792810a812835
Titus Kanogo Njema <njema3@gmail.com>
2025-06-08 19:19:22 +0300

    first commit

---
+++ stack.py
@@ -0,0 +1,17 @@
+class Stack:
+    def __init__(self):
+        self.data = []
+
+    def push(self, item):
+        return self.data.append(item)
+    
+    def pop(self):
+        return self.data.pop()
+    
+    def is_empty(self):
+        return len(self.data) == 0
+    
+    def peek(self):
+        return self.data[-1]
+
+    
