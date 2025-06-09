f6acec4e9a9ee10dd60229667e5a4a06ca8fb210
Titus Kanogo Njema <njema3@gmail.com>
2025-06-09 19:47:15 +0300

    new changes

--- Sample.py
+++ Sample.py
@@ -2,3 +2,9 @@ from stack import Stack
 
 def parentheses_checker(expr):
     s = Stack()
+    lefty = '[{(' 
+    righty = ']})'
+    for c in expr:
+        if c in lefty:
+            s.push(c)
+            
